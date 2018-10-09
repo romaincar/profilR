08596260b0cd4a28e6180b957d3287ef34713dee
rom <c59.romain@live.fr>
2018-10-09 16:41:00 +0200

    js

---
+++ untitled2/cvR.js
@@ -0,0 +1,24 @@
+function fonctionclicks() {
+    var x = document.getElementById("exp");
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function fonctionclick() {
+    var z = document.getElementById("form");
+    if (z.style.display === 'none') {
+        z.style.display = 'block';
+    } else {
+        z.style.display = 'none';
+    }
+}
+function fonctionclicke() {
+    var y = document.getElementById("compe");
+    if (y.style.display === 'none') {
+        y.style.display = 'block';
+    } else {
+        y.style.display = 'none';
+    }
+}
