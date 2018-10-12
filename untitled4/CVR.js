c3fba733f03f07dc9fd4e984492bc4d0985b954d
rom <c59.romain@live.fr>
2018-10-12 14:44:57 +0200

    html+css+js+bootstrap

---
+++ untitled4/CVR.js
@@ -0,0 +1,39 @@
+function fonctionclicker() {
+    var x = document.getElementById("coordonnées");
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById("button").innerHTML="-";
+    } else {
+        x.style.display = 'none';
+        document.getElementById("button").innerHTML="+";
+    }}
+
+function fonctionclick() {
+    var x = document.getElementById("experiences");
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById("button1").innerHTML="-";
+    } else {
+        x.style.display = 'none';
+        document.getElementById("button1").innerHTML="+";
+    }}
+
+function fonctionclicke() {
+    var x = document.getElementById("formations");
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById("button2").innerHTML="-";
+    } else {
+        x.style.display = 'none';
+        document.getElementById("button2").innerHTML="+";
+    }}
+
+function fonctionclicks() {
+    var x = document.getElementById("logiciel");
+    if (x.style.display === 'none') {
+        x.style.display = '';
+        document.getElementById("button3").innerHTML="-";
+    } else {
+        x.style.display = 'none';
+        document.getElementById("button3").innerHTML="+";
+    }}
